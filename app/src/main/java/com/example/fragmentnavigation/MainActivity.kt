b278aeb89b3ea80d5c155956113f60384eda6319
Ivan Voloshyn <ivan.voloshyn.fs@gmail.com>
2023-08-21 18:47:04 +0200

    Finish Ui part with resources

--- app/src/main/java/com/example/fragmentnavigation/MainActivity.kt
+++ app/src/main/java/com/example/fragmentnavigation/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.fragmentnavigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fragmentnavigation.databinding.ActivityMainBinding
+import com.example.fragmentnavigation.fragments.AboutFragment
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, AboutFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/fragmentnavigation/fragments/AboutFragment.kt
@@ -0,0 +1,7 @@
+package com.example.fragmentnavigation.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fragmentnavigation.R
+
+class AboutFragment : Fragment(R.layout.fragment_about) {
+}
